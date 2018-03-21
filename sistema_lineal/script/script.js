113ff60d064e6de7cd16550cc5e5bf04c4c44408
Valentina Tironi <ValentinaTironi@users.noreply.github.com>
2018-03-21 08:56:16 -0300

    casi terminado

--- sistema_lineal/script/script.js
+++ sistema_lineal/script/script.js
@@ -5,6 +5,7 @@ $( "#submit" ).click(function() {
 
 function main() {
   //set variables
+  $( ".result" ).show();
   var operator_one = $( "select#one").val();
   var operator_two = $( "select#two").val();
   var x1 = $("input[type=number][name=mult_x_one]").val();
@@ -20,30 +21,42 @@ function main() {
   var r1 = $("input[type=number][name=result_one]").val();
   var r2 = $("input[type=number][name=result_two]").val();;
   var x, y;
+  var _void = "";
 
-  //new values equation_one
-  negative_x2 = -x2;
-  var new_x1 = x1*negative_x2;
-  var new_y1 = y1*negative_x2;
-  var new_r1 = r1*negative_x2;
-
-  //new values equation_two
-  var new_x2 = x2*x1;
-  var new_y2 = y2*x1;
-  var new_r2 = r2*x1;
-
-  y = value_y(new_y1, new_y2, new_r1, new_r2, operator_one);
-  x = value_x(new_x1, new_y1, new_r1, operator_one, y);
-
-  //show result
-  $( ".result" ).show();
-  console.log(x);
-  if (isNaN(x)) {
-    $( "#result" ).html("Son iguales");
+  //not void
+  var matches = 0;
+  $("input").each(function(i, val) {
+    if ($(this).val() == '') {
+      $(this).addClass("has-error");
+      matches++;
+    }else {
+      $(this).removeClass("has-error");
+    }
+  });
+  if (0 < matches && matches <= 6) {
+    $( "#result" ).html("Debe ingresar el código que falta");
   } else {
-    $( "#result" ).html("( " + x + ", " + y + ")");
+    //new values equation_one
+    negative_x2 = -x2;
+    var new_x1 = x1*negative_x2;
+    var new_y1 = y1*negative_x2;
+    var new_r1 = r1*negative_x2;
+
+    //new values equation_two
+    var new_x2 = x2*x1;
+    var new_y2 = y2*x1;
+    var new_r2 = r2*x1;
+
+    y = value_y(new_y1, new_y2, new_r1, new_r2, operator_one);
+    x = value_x(new_x1, new_y1, new_r1, operator_one, y);
+
+    //show result
+    if (y == undefined) {
+      $( "#result" ).html("Son iguales");
+    } else {
+      $( "#result" ).html("(" + x + ", " + y + ")");
+    }
   }
-  // value_x(new_x1, new_y1, new_r1, operator_one, y)
 };
 
 //Result value of y
@@ -56,9 +69,9 @@ function value_y(new_y1, new_y2, new_r1, new_r2, operator_one) {
   }
   r_total = (new_r1) + (new_r2);
   if (r_total == 0 && result_y == 0) {
-    y = 0;
-  } else if (r_total == 0) {
     y = undefined;
+  } else if (r_total == 0) {
+    y = 0;
   } else {
     y = r_total / result_y;
   }
