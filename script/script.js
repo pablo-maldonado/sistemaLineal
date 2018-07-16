62d7f6d21eb4561181aa732edb07edc039c65b54
Valentina Tironi <ValentinaTironi@users.noreply.github.com>
2018-07-16 09:23:12 -0300

    add item menu

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
     $('#sistemasLineales').attr('href', 'sistema_lineal/index.html');
     $('#ecuacionesRecta').attr('href', 'ecuaciones_recta/index.html');
+    $('#limites').attr('href', 'limites/index.html');
 });
